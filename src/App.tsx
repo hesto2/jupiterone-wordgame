11c9bd873cac3af206a577cb0d74dfafc25a960f
hesto2 <hestermancarter@gmail.com>
2022-02-03 11:50:54 -0700

    init 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import {
-  InformationCircleIcon,
-  ChartBarIcon,
-  SunIcon,
-} from '@heroicons/react/outline'
-import { useState, useEffect } from 'react'
-import { Alert } from './components/alerts/Alert'
-import { Grid } from './components/grid/Grid'
-import { Keyboard } from './components/keyboard/Keyboard'
-import { AboutModal } from './components/modals/AboutModal'
-import { InfoModal } from './components/modals/InfoModal'
-import { StatsModal } from './components/modals/StatsModal'
+import { InformationCircleIcon, ChartBarIcon } from '@heroicons/react/outline';
+import { useState, useEffect } from 'react';
+import { Alert } from './components/alerts/Alert';
+import { Grid } from './components/grid/Grid';
+import { Keyboard } from './components/keyboard/Keyboard';
+import { AboutModal } from './components/modals/AboutModal';
+import { InfoModal } from './components/modals/InfoModal';
+import { StatsModal } from './components/modals/StatsModal';
 import {
   GAME_TITLE,
   WIN_MESSAGES,
@@ -18,144 +14,134 @@ import {
   NOT_ENOUGH_LETTERS_MESSAGE,
   WORD_NOT_FOUND_MESSAGE,
   CORRECT_WORD_MESSAGE,
-} from './constants/strings'
-import { isWordInWordList, isWinningWord, solution } from './lib/words'
-import { addStatsForCompletedGame, loadStats } from './lib/stats'
+} from './constants/strings';
+import { isWordInWordList, isWinningWord, solution } from './lib/words';
+import { addStatsForCompletedGame, loadStats } from './lib/stats';
 import {
   loadGameStateFromLocalStorage,
   saveGameStateToLocalStorage,
-} from './lib/localStorage'
+} from './lib/localStorage';
 
-import './App.css'
+import './App.css';
 
-const ALERT_TIME_MS = 2000
+const ALERT_TIME_MS = 2000;
 
 function App() {
   const prefersDarkMode = window.matchMedia(
     '(prefers-color-scheme: dark)'
-  ).matches
+  ).matches;
 
-  const [currentGuess, setCurrentGuess] = useState('')
-  const [isGameWon, setIsGameWon] = useState(false)
-  const [isInfoModalOpen, setIsInfoModalOpen] = useState(false)
-  const [isAboutModalOpen, setIsAboutModalOpen] = useState(false)
-  const [isNotEnoughLetters, setIsNotEnoughLetters] = useState(false)
-  const [isStatsModalOpen, setIsStatsModalOpen] = useState(false)
-  const [isWordNotFoundAlertOpen, setIsWordNotFoundAlertOpen] = useState(false)
-  const [isGameLost, setIsGameLost] = useState(false)
-  const [isDarkMode, setIsDarkMode] = useState(
-    localStorage.getItem('theme')
-      ? localStorage.getItem('theme') === 'dark'
-      : prefersDarkMode
-      ? true
-      : false
-  )
-  const [successAlert, setSuccessAlert] = useState('')
+  const [currentGuess, setCurrentGuess] = useState('');
+  const [isGameWon, setIsGameWon] = useState(false);
+  const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
+  const [isAboutModalOpen, setIsAboutModalOpen] = useState(false);
+  const [isNotEnoughLetters, setIsNotEnoughLetters] = useState(false);
+  const [isStatsModalOpen, setIsStatsModalOpen] = useState(false);
+  const [isWordNotFoundAlertOpen, setIsWordNotFoundAlertOpen] = useState(false);
+  const [isGameLost, setIsGameLost] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [successAlert, setSuccessAlert] = useState('');
   const [guesses, setGuesses] = useState<string[]>(() => {
-    const loaded = loadGameStateFromLocalStorage()
+    const loaded = loadGameStateFromLocalStorage();
     if (loaded?.solution !== solution) {
-      return []
+      return [];
     }
-    const gameWasWon = loaded.guesses.includes(solution)
+    const gameWasWon = loaded.guesses.includes(solution);
     if (gameWasWon) {
-      setIsGameWon(true)
+      setIsGameWon(true);
     }
     if (loaded.guesses.length === 6 && !gameWasWon) {
-      setIsGameLost(true)
+      setIsGameLost(true);
     }
-    return loaded.guesses
-  })
+    return loaded.guesses;
+  });
 
-  const [stats, setStats] = useState(() => loadStats())
+  const [stats, setStats] = useState(() => loadStats());
 
   useEffect(() => {
     if (isDarkMode) {
-      document.documentElement.classList.add('dark')
+      document.documentElement.classList.add('dark');
     } else {
-      document.documentElement.classList.remove('dark')
+      document.documentElement.classList.remove('dark');
     }
-  }, [isDarkMode])
+  }, [isDarkMode]);
 
   const handleDarkMode = (isDark: boolean) => {
-    setIsDarkMode(isDark)
-    localStorage.setItem('theme', isDark ? 'dark' : 'light')
-  }
+    setIsDarkMode(isDark);
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  };
 
   useEffect(() => {
-    saveGameStateToLocalStorage({ guesses, solution })
-  }, [guesses])
+    saveGameStateToLocalStorage({ guesses, solution });
+  }, [guesses]);
 
   useEffect(() => {
     if (isGameWon) {
       setSuccessAlert(
         WIN_MESSAGES[Math.floor(Math.random() * WIN_MESSAGES.length)]
-      )
+      );
       setTimeout(() => {
-        setSuccessAlert('')
-        setIsStatsModalOpen(true)
-      }, ALERT_TIME_MS)
+        setSuccessAlert('');
+        // setIsStatsModalOpen(true);
+      }, ALERT_TIME_MS);
     }
     if (isGameLost) {
       setTimeout(() => {
-        setIsStatsModalOpen(true)
-      }, ALERT_TIME_MS)
+        setIsStatsModalOpen(true);
+      }, ALERT_TIME_MS);
     }
-  }, [isGameWon, isGameLost])
+  }, [isGameWon, isGameLost]);
 
   const onChar = (value: string) => {
     if (currentGuess.length < 5 && guesses.length < 6 && !isGameWon) {
-      setCurrentGuess(`${currentGuess}${value}`)
+      setCurrentGuess(`${currentGuess}${value}`);
     }
-  }
+  };
 
   const onDelete = () => {
-    setCurrentGuess(currentGuess.slice(0, -1))
-  }
+    setCurrentGuess(currentGuess.slice(0, -1));
+  };
 
   const onEnter = () => {
     if (isGameWon || isGameLost) {
-      return
+      return;
     }
     if (!(currentGuess.length === 5)) {
-      setIsNotEnoughLetters(true)
+      setIsNotEnoughLetters(true);
       return setTimeout(() => {
-        setIsNotEnoughLetters(false)
-      }, ALERT_TIME_MS)
+        setIsNotEnoughLetters(false);
+      }, ALERT_TIME_MS);
     }
 
     if (!isWordInWordList(currentGuess)) {
-      setIsWordNotFoundAlertOpen(true)
+      setIsWordNotFoundAlertOpen(true);
       return setTimeout(() => {
-        setIsWordNotFoundAlertOpen(false)
-      }, ALERT_TIME_MS)
+        setIsWordNotFoundAlertOpen(false);
+      }, ALERT_TIME_MS);
     }
 
-    const winningWord = isWinningWord(currentGuess)
+    const winningWord = isWinningWord(currentGuess);
 
     if (currentGuess.length === 5 && guesses.length < 6 && !isGameWon) {
-      setGuesses([...guesses, currentGuess])
-      setCurrentGuess('')
+      setGuesses([...guesses, currentGuess]);
+      setCurrentGuess('');
 
       if (winningWord) {
-        setStats(addStatsForCompletedGame(stats, guesses.length))
-        return setIsGameWon(true)
+        setStats(addStatsForCompletedGame(stats, guesses.length));
+        return setIsGameWon(true);
       }
 
       if (guesses.length === 5) {
-        setStats(addStatsForCompletedGame(stats, guesses.length + 1))
-        setIsGameLost(true)
+        setStats(addStatsForCompletedGame(stats, guesses.length + 1));
+        setIsGameLost(true);
       }
     }
-  }
+  };
 
   return (
     <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div className="flex w-80 mx-auto items-center mb-8 mt-12">
         <h1 className="text-xl grow font-bold dark:text-white">{GAME_TITLE}</h1>
-        <SunIcon
-          className="h-6 w-6 cursor-pointer dark:stroke-white"
-          onClick={() => handleDarkMode(!isDarkMode)}
-        />
         <InformationCircleIcon
           className="h-6 w-6 cursor-pointer dark:stroke-white"
           onClick={() => setIsInfoModalOpen(true)}
@@ -184,8 +170,8 @@ function App() {
         isGameLost={isGameLost}
         isGameWon={isGameWon}
         handleShare={() => {
-          setSuccessAlert(GAME_COPIED_MESSAGE)
-          return setTimeout(() => setSuccessAlert(''), ALERT_TIME_MS)
+          setSuccessAlert(GAME_COPIED_MESSAGE);
+          return setTimeout(() => setSuccessAlert(''), ALERT_TIME_MS);
         }}
       />
       <AboutModal
@@ -193,13 +179,13 @@ function App() {
         handleClose={() => setIsAboutModalOpen(false)}
       />
 
-      <button
+      {/* <button
         type="button"
         className="mx-auto mt-8 flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 select-none"
         onClick={() => setIsAboutModalOpen(true)}
       >
         {ABOUT_GAME_MESSAGE}
-      </button>
+      </button> */}
 
       <Alert message={NOT_ENOUGH_LETTERS_MESSAGE} isOpen={isNotEnoughLetters} />
       <Alert
@@ -213,7 +199,7 @@ function App() {
         variant="success"
       />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
