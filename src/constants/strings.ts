bbd48210d416566210acd1a20b6530b88fcb7f24
hesto2 <hestermancarter@gmail.com>
2022-02-04 12:22:33 -0700

    update text

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,4 +1,4 @@
-export const GAME_TITLE = '';
+export const GAME_TITLE = 'J1 Word Game';
 
 export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!'];
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard';
