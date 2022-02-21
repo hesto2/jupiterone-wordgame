ec8809d53b39526b96c2cf9fe7cd308bb1c7b18b
hasard1 <ardit@bigorangelab.com>
2022-02-21 20:32:33 +0100

    fix lint

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -7,39 +7,36 @@ export const shareStatus = async (
   lost: boolean,
   wordId: number | null
 ) => {
-  
+
   var iOS = /(iP*)/g.test(navigator.userAgent)
   const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-  console.log(isSafari,'isSafari')
-  if(!isSafari) {
-  const emojiiGrid = await generateEmojiGrid(guesses);
+  console.log(isSafari, 'isSafari')
+  if (!isSafari) {
+    const emojiiGrid = await generateEmojiGrid(guesses);
 
     navigator.clipboard.writeText(
-        `JupiterOne Cybersecurity Word Game ${wordId || ''} ${
-          lost ? '6' : guesses.length
-        }/6\n\n` +
-          emojiiGrid +
-          '\n\n Play free: https://www.jupiterone.com/wordgame'
-      );
+      `JupiterOne Cybersecurity Word Game ${wordId || ''} ${lost ? '6' : guesses.length}/6\n\n` +
+      emojiiGrid +
+      '\n\n Play free: https://www.jupiterone.com/wordgame'
+    );
   } else {
     const bringResponse = async () => {
       const response = await generateEmojiGrid(guesses);
       return (
-        `JupiterOne Cybersecurity Word Game ${wordId || ''} ${
-        lost ? '6' : guesses.length
-      }/6\n\n` +
-      response +
-        '\n\n Play free: https://www.jupiterone.com/wordgame') ;
+        `JupiterOne Cybersecurity Word Game ${wordId || ''} ${lost ? '6' : guesses.length
+        }/6\n\n` +
+        response +
+        '\n\n Play free: https://www.jupiterone.com/wordgame');
+    }
+    console.log('navigator', navigator)
+    navigator.clipboard.write([new ClipboardItem({ "text/plain": bringResponse() })])
+      .then(function () { console.log('copied'); })
+      .catch(function (error) { console.log(error); });
   }
-  console.log('navigator',navigator)
-  navigator.clipboard.write([new ClipboardItem({ "text/plain": bringResponse() })])
-    .then(function () { console.log('copied'); })
-    .catch(function (error) { console.log(error); });
-  }
- 
- 
 
-  
+
+
+
 }
 export const generateEmojiGrid = async (guesses: string[]) => {
   const promises = guesses.map(async (guess) => {
