6cbce6a75f1c7980332f6ac8a108c3ec0f9676db
hasard1 <ardit@bigorangelab.com>
2022-02-21 20:29:46 +0100

    fix safari share issue

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -2,22 +2,45 @@ import { getGuessStatuses } from './statuses';
 import { solutionIndex } from './words';
 import { GAME_TITLE } from '../constants/strings';
 import { submitWord } from '../api';
-
 export const shareStatus = async (
   guesses: string[],
   lost: boolean,
   wordId: number | null
 ) => {
+  
+  var iOS = /(iP*)/g.test(navigator.userAgent)
+  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+  console.log(isSafari,'isSafari')
+  if(!isSafari) {
   const emojiiGrid = await generateEmojiGrid(guesses);
-  navigator.clipboard.writeText(
-    `JupiterOne Cybersecurity Word Game ${wordId || ''} ${
-      lost ? '6' : guesses.length
-    }/6\n\n` +
-      emojiiGrid +
-      '\n\n Play free: https://www.jupiterone.com/wordgame'
-  );
-};
 
+    navigator.clipboard.writeText(
+        `JupiterOne Cybersecurity Word Game ${wordId || ''} ${
+          lost ? '6' : guesses.length
+        }/6\n\n` +
+          emojiiGrid +
+          '\n\n Play free: https://www.jupiterone.com/wordgame'
+      );
+  } else {
+    const bringResponse = async () => {
+      const response = await generateEmojiGrid(guesses);
+      return (
+        `JupiterOne Cybersecurity Word Game ${wordId || ''} ${
+        lost ? '6' : guesses.length
+      }/6\n\n` +
+      response +
+        '\n\n Play free: https://www.jupiterone.com/wordgame') ;
+  }
+  console.log('navigator',navigator)
+  navigator.clipboard.write([new ClipboardItem({ "text/plain": bringResponse() })])
+    .then(function () { console.log('copied'); })
+    .catch(function (error) { console.log(error); });
+  }
+ 
+ 
+
+  
+}
 export const generateEmojiGrid = async (guesses: string[]) => {
   const promises = guesses.map(async (guess) => {
     const status = await submitWord(guess);
@@ -36,6 +59,5 @@ export const generateEmojiGrid = async (guesses: string[]) => {
       .join('');
   });
   const results = await Promise.all(promises);
-  console.log('results', results);
   return results.join('\n');
 };
