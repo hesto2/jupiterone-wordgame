59c5224a52d2e7b3b77293d8903673275f06ecb6
A S <as@As-MacBook-Pro.local>
2022-02-24 23:35:00 +0100

    revert word check

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -4,8 +4,7 @@ import { VALID_GUESSES } from '../constants/validGuesses'
 
 export const isWordInWordList = (word: string) => {
   return (
-    WORDS.includes(word.toLowerCase()) ||
-    VALID_GUESSES.includes(word.toLowerCase())
+    true
   )
 }
 
